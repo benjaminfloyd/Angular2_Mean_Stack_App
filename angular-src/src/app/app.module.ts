1c300ceb3a9ab320c85d406a787238f220e301a2
benjaminfloyd <benjamin.floyd@me.com>
2017-09-01 12:53:58 -0400

    Set ups services and Register

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -11,8 +11,13 @@ import { RegisterComponent } from './Components/register/register.component';
 import { HomeComponent } from './Components/home/home.component';
 import { DashboardComponent } from './Components/dashboard/dashboard.component';
 import { ProfileComponent } from './Components/profile/profile.component';
+import { RouterLinkActive } from '@angular/router';
+import {ValidateService} from './services/validate.service';
+import {AuthService} from './services/auth.service';
+import {FlashMessagesModule} from 'angular2-flash-messages';
+import {AuthGuard} from './guards/auth.guard';
 
-const appRoutes: Routes = [
+const appRoutes:Routes = [
   {path:'', component: HomeComponent},
   {path:'register', component: RegisterComponent}, 
   {path:'login', component: LoginComponent},  {path: 'dashboard', component: DashboardComponent},
@@ -35,9 +40,10 @@ const appRoutes: Routes = [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    FlashMessagesModule
   ],
-  providers: [],
+  providers: [ValidateService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
