f9dce833a2afe861408e699ce59d2334ed746200
benjaminfloyd <benjamin.floyd@me.com>
2017-09-03 16:52:54 -0400

    fixing mlab

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://admin:12345678@ds123084.mlab.com:23084/mean2authapp',
+    database: 'mongodb://admin:12345@ds123084.mlab.com:23084/mean2authapp',
     secret: 'yoursecert'
 }
